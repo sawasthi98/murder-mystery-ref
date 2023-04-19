376d6e4462c4ac53afdbaaf66483402ab065ed85
sawasthi98 <113808115+sawasthi98@users.noreply.github.com>
2023-04-18 20:24:26 -0400

    initial

---
+++ murder mystery game/js/main.js
@@ -0,0 +1,5 @@
+const doll = document.querySelector('#doll').addEventListener('click',pickUpDoll)
+
+function pickUpDoll(){
+    alert("Upon closer inspection, one of the eyes is a camera. When you pull the string, it begins to sing a song.")
+}
